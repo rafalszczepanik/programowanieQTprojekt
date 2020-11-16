abbef4470488c4ccdf8179f5c3aa8d26ff82958a
rafalszczepanik <rafal.szczepanik17@gmail.com>
2020-11-16 22:11:10 +0100

    Add designer file without convert

--- Projekt Programowanie QT/main.py
+++ Projekt Programowanie QT/main.py
@@ -1,8 +1,7 @@
-# This is a sample Python script.
-
+# This is a sample Python scr
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+import PyQt5
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
